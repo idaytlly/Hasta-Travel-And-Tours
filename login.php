24f37107b32dd5fcca474ac12ab8bea332a80127
IDA YATULLAILIYEH BINTI AMRUN <idayatullailiyeh@graduate.utm.my>
2025-12-09 03:26:50 +0800

    Update login.php

--- login.php
+++ login.php
@@ -3,9 +3,6 @@
 ?>
 
 <div class="container my-5">
-    <form method="post" action="loginprocess.php">
-  
-    <fieldset>
   <div class="row justify-content-center">
     <div class="col-md-6">
       <div class="card shadow-sm">
@@ -36,7 +33,6 @@
       </div>
     </div>
   </div>
-</fieldset>
 </div>
 
 <?php
