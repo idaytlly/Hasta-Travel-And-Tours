2146a40d36af526f1b0c59930f6219716e595fb4
Nur Alia Athirah Binti Suzuddin <aliaathirah005@gmail.com>
2025-12-09 05:02:32 +0800

    vehicles

--- header_login.php
+++ header_login.php
@@ -60,12 +60,26 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarLogin">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="index.php">Dashboard</a></li>
-        <li class="nav-item"><a class="nav-link" href="index.php">Vehicle Listing</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Book Vehicle</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">History</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Settings</a></li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Vehicle Listing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Book Vehicle</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">History</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Settings</a>
+        </li>
       </ul>
     </div>
   </div>
