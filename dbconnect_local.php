11c953ec30e0dd1f29012995260ead493101b397
IDA YATULLAILIYEH BINTI AMRUN <idayatullailiyeh@graduate.utm.my>
2025-12-09 03:25:08 +0800

    update database and login

---
+++ dbconnect_local.php
@@ -0,0 +1,13 @@
+<?php
+// DB parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_hasta";
+
+// Create Connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+
+
+//Verify Connection
+?>
