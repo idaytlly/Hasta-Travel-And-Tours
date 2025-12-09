734afd86f326f48897f6696e8cf6bf8b69278e37
IDA YATULLAILIYEH BINTI AMRUN <idayatullailiyeh@graduate.utm.my>
2025-12-09 10:39:28 +0800

    update header

--- header_content.php
+++ header_content.php
@@ -23,7 +23,6 @@
       flex-direction: column;
     }
 
-
     .navbar-logo {
       max-height: 60px; 
       width: auto;     
@@ -46,8 +45,7 @@
     <div class="collapse navbar-collapse" id="navbarLogin">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="index.php">-Test</a></li>
-        <li class="nav-item"><a class="nav-link" href="index.php">Vehicle Listing</a></li>
+        <li class="nav-item"><a class="nav-link" href="index.php">Dashboard</a></li>
         <li class="nav-item"><a class="nav-link" href="#">Book Vehicle</a></li>
         <li class="nav-item"><a class="nav-link" href="#">History</a></li>
         <li class="nav-item"><a class="nav-link" href="#">Settings</a></li>
